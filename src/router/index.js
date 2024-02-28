7c2f5a749111c75df8837b240f25dacc939a20cf
Nikolay Surodin <kolya_12.93@mail.ru>
2024-02-28 13:48:29 +0700

    Replaced import FilmsView component.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useMeStore } from '../stores/meStore'
 import FilmItem from '../views/FilmItem.vue'
 import HomeView from '../views/HomeView.vue'
+import FilmsView from '../views/FilmsView.vue'
 
 const router = createRouter( {
   history: createWebHistory( import.meta.env.BASE_URL ),
@@ -14,7 +15,7 @@ const router = createRouter( {
     {
       path: '/films',
       name: 'films',
-      component: () => import('../views/FilmsView.vue')
+      component: FilmsView
     },
     {
       path: '/tv-shows',
